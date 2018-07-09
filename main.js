056564019e16dd5dba7ad4e6391504dab9b75b43
Connor <connor.j.byrnes@gmail.com>
2018-07-08 23:31:33 -0400

    Added files

---
+++ main.js
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 800, 
+    height: 600,
+    titleBarStyle: 'hiddenInset',
+    title: 'HLTV',
+    icon: 'assets/icon.png'})
+
+  // and load the index.html of the app.
+
+  win.loadURL('https://hltv.org')
+  win.webContents.on('did-finish-load', function() {
+    win.webContents.insertCSS('html, .navbar{-webkit-app-region: drag; height: 40px; padding-top: 40px; !important;}',1)
+  });
+}
+
+app.on('ready', createWindow)
